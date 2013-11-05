e846b254bd64579417fd78aa0f709c07c18f9466
Jeremy Archer <open-source@fatlotus.com>
2013-11-05 17:50:51 -0600

    Add setup.py to repo.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='Headhunter.d',
+  version='0.1.1',
+  description='Minimal job scheduler written with AMQP.',
+  author='Jeremy Archer',
+  author_email='open-source@fatlotus.com',
+  url='https://www.github.com/fatlotus/headhunter.d',
+  packages=['headhunter'],
+)
